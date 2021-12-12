f38845c94c24d3306aee84ae055c30fce8c93186
AarSchoSkIg <aaronschomer@yahoo.com>
2021-12-11 23:54:19 -0500

    Created a maven project called Project0. Added a package for java beans...well bean singular for Bicycle class. Bicycle class was initialized with fields for brand, model, color, and terrainType. Added a driver class for the application package with a main method.

---
+++ Project0/src/main/java/com/revature/beans/Bicylcle.java
@@ -0,0 +1,16 @@
+package com.revature.beans;
+
+public class Bicylcle {
+	private String brand;
+	private String model;
+	private String color;
+	private String terrainType;
+	
+	public Bicylcle () {
+		brand = "Brand";
+		model = "Model";
+		color = "black";
+		terrainType = "";
+	}
+	
+}
