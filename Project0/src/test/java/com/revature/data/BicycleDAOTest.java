d67af36686e1f86ff3b58cef4651f8a81204d8d2
AarSchoSkIg <aaronschomer@yahoo.com>
2021-12-19 17:40:33 -0500

    Created beginnings of BicyclePostgress class, override methods getById, getAll, update, delete, and create. created a test getByIdwhenIdExists in BicycleDAOTest class

--- Project0/src/test/java/com/revature/data/BicycleDAOTest.java
+++ Project0/src/test/java/com/revature/data/BicycleDAOTest.java
@@ -1,6 +1,22 @@
 package com.revature.data;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import com.revature.beans.Bicycle;
+
 public class BicycleDAOTest {
 	
-	private 
+	private BicycleDAO bicycleDAO = new BicyclePostgres();
+	
+	@Test // Test 
+	public void getByIdWhenIdExists() {
+		// setup
+		int idInput = 1;
+		// call idOutput method for testing
+		Bicycle idOutput = bicycleDAO.getById(idInput);
+		// assert that it did what it is expected to do
+		assertEquals(1, idOutput.getId());
+	}
 }
---
+++ Project0/src/test/java/com/revature/data/BicyclePostgres.java
@@ -0,0 +1,37 @@
+package com.revature.data;
+
+import java.util.Set;
+
+public class BicyclePostgres implements BicycleDAO {
+
+	@Override
+	public int create(Object dataToAdd) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<?> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void update(Object dataToUpdate) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void delete(Object dataToDelete) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
