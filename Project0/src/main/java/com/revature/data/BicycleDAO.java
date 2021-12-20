233d08853a9ca4ce7feebf892ca5960c7869f3fa
AarSchoSkIg <aaronschomer@yahoo.com>
2021-12-19 22:44:34 -0500

    Added try catch block prepared statement to BicyclePostgress class. deleted person.java class.

--- Project0/src/main/java/com/revature/data/BicycleDAO.java
+++ Project0/src/main/java/com/revature/data/BicycleDAO.java
@@ -1,13 +1,15 @@
 package com.revature.data;
 
 import java.util.Set;
+import com.revature.beans.Bicycle;
 
-public interface BicycleDAO<Bicycle> {
+public interface BicycleDAO {
 	
 	public int create(Bicycle dataToAdd);
 	public Bicycle getById(int id);
 	public Set<Bicycle> getAll();
 	public void update(Bicycle dataToUpdate);
 	public void delete(Bicycle dataToDelete);
+	
 
 }
--- Project0/src/main/java/com/revature/services/ServicesForUsers.java
+++ Project0/src/main/java/com/revature/services/ServicesForUsers.java
@@ -5,11 +5,9 @@ import java.util.Set;
 import com.revature.beans.Bicycle;
 
 public interface ServicesForUsers {
-	/*Users are able to search for Bikes by model, color, Id number, Terrain Type, login, 
-	 * update User information, and add new bikes.
+	/*Users are able to search for Bikes by model, color, Id number, Terrain Type, 
+	 * update Bike information, and add new bikes.
 	 */
-	public Person register(Person newUser);
-	public Person logIn(String username, String password);
 	public Bicycle updateBicycle(Bicycle bicycleToUpdate);
 	public Bicycle addNewBicycle(int bicycleId, Bicycle newBicycle);
 	public Set<Bicycle> viewAvailableBicycles();
